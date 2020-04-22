7d69cd9b84fbe510d175a463b10825c9c9ad6d52
Antonio Jackson <antoniojackson@gmail.com>
2020-04-22 09:22:24 -0600

    Adding test_restock

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -22,4 +22,10 @@ class PantryTest < Minitest::Test
   def test_stock_check
     assert_equal 0, @pantry.stock_check(@ingredient1)
   end
+
+  def test_restock
+    @pantry.restock(@ingredient1, 5)
+    @pantry.restock(@ingredient1, 10)
+    assert_equal 15, @pantry.stock_check(ingredient1)
+  end
 end
