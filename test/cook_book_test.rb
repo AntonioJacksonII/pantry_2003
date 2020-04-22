15353ba36b6497196c8639692f9fb7035a1858e2
Antonio Jackson <antoniojackson@gmail.com>
2020-04-22 09:53:35 -0600

    Adding test_highest_calorie_meal

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -31,4 +31,20 @@ class CookBookTest < Minitest::Test
     @cookbook.add_recipe(@recipe2)
     assert_equal [@recipe1, @recipe2], @cookbook.recipes
   end
+
+  def test_highest_calorie_meal
+    cookbook = CookBook.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+    recipe2 = Recipe.new("Cheese Burger")
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    recipe2.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
+    assert_equal recipe2, cookbook.highest_calorie_meal
+  end
 end
