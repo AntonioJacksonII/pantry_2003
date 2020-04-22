9bec57fe7615cded8639837e3e0eef742703324d
Antonio Jackson <antoniojackson@gmail.com>
2020-04-22 09:18:34 -0600

    Adding stock attr_reader

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -7,7 +7,7 @@ class PantryTest < Minitest::Test
 
   def setup
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
-    # @ingredient2 = Ingredient.new("Macaroni", "oz", 200)
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
     @pantry = Pantry.new
   end
 
