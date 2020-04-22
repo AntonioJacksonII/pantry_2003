3643b36d562fefe0ef3de429efead7ec9c072476
Antonio Jackson <antoniojackson@gmail.com>
2020-04-22 09:08:54 -0600

    Adding attr_reader methods

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,5 +1,9 @@
 class Ingredient
+  attr_reader :name, :unit, :calories
 
   def initialize(ingredient)
+    @name = ingredient[:name]
+    @unit = ingredient[:unit]
+    @calories = ingredient[:calories]
   end
 end
